a6117d6d1cdfcfbbcbfba56d7ff454057f6db644
omrgrcn <omergercin@gmail.com>
2019-04-05 19:28:08 +0300

    add new routes

--- routes/movie.js
+++ routes/movie.js
@@ -4,11 +4,55 @@ const router = express.Router();
 // Models
 const Movie = require('../models/Movie');
 
+router.get('/', (req, res) => {
+  const promise = Movie.find({ });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1 });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+router.get('/:movie_id', (req,res,next) => {
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.', code: 1 });
+
+    res.json(movie);
+  }).catch((err) => {
+    console.log('error log')
+    res.json(err);
+  });
+});
+
+router.put('/:movie_id', (req,res,next) => {
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, { new: true });
+  promise.then((movie) => {
+    if(!movie)
+      next({ message: 'The movie was not found.', code: 1 });
+
+    res.json(movie);
+  }).catch((err) => {
+    console.log('error log')
+    res.json(err);
+  });
+});
+
 router.post('/', (req, res, next) => {
   const movie = new Movie(req.body);
 
 /*
-   const { title, imdb_score, category, country, year } = req.body;
+  const { title, imdb_score, category, country, year } = req.body;
   
   const movie = new Movie({
     title: title,
