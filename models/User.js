bb56aee72a65d9cc8fd46fee901078b1f3ac7c01
omrgrcn <omergercin@gmail.com>
2019-04-08 18:39:26 +0300

    /register POST Method

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    //user_id = Schema.Types.ObjectId,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function(req, res, next) {
+  const { username, password } = req.body;
+  const user = new User({
+    username,
+    password
+  });
+
+  const promise = user.save();
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
