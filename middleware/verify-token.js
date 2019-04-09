2cb0adf2e624161e4cd1f8946bc335a3077f85c1
omrgrcn <omergercin@gmail.com>
2019-04-09 18:01:28 +0300

    jsonwebtoken verify

---
+++ middleware/verify-token.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.headers['x-access-token'] || req.body.token || req.query.token
+
+    if(token) {
+        jwt.verify(token, req.app.get('api_secret_key'), (err, decoded) => {
+            if(err) {
+                res.json({
+                    status: 2,
+                    message: 'Failed to authenticate token.'
+                })
+            } else {
+                console.log('giris basarili - decoded :', decoded);
+                req.decode = decoded;
+                next();
+            }
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: 'No token provided.'
+        })
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -29,42 +29,42 @@ router.post('/register', function(req, res, next) {
 });
 
 router.post('/authenticate', (req, res) => {
-	const { username, password } = req.body;
+  const { username, password } = req.body;
 
-	User.findOne({
-		username
-	}, (err, user) => {
-		if (err)
-			throw err;
+  User.findOne({
+    username
+  }, (err, user) => {
+    if (err)
+      throw err;
 
-		if(!user){
-			res.json({
-				status: false,
-				message: 'Authentication failed, user not found.'
-			});
-		}else{
-			bcrypt.compare(password, user.password).then((result) => {
-				if (!result){
-					res.json({
-						status: false,
-						message: 'Authentication failed, wrong password.'
-					});
-				}else{
-					const payload = {
-						username
-					};
-					const token = jwt.sign(payload, req.app.get('api_secret_key'), {
-						expiresIn: 720 // 12 saat
-					});
+    if(!user){
+      res.json({
+        status: false,
+        message: 'Authentication failed, user not found.'
+      });
+    } else {
+      bcrypt.compare(password, user.password).then((result) => {
+        if (!result){
+          res.json({
+            status: false,
+            message: 'Authentication failed, wrong password.'
+          });
+        }else{
+          const payload = {
+            username
+          };
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn: 720 // 12 saat
+          });
 
-					res.json({
-						status: true,
-						token
-					})
-				}
-			});
-		}
-	});
+          res.json({
+            status: true,
+            token
+          })
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
